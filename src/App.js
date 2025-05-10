8f6af57c60843a5cb611b13b0b1d0bdd6069df93
Suraj Singh <surajsingh@192.168.1.5>
2025-05-10 22:23:59 +0530

    homePage of Website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomePage from './components/pages/homePage';
+import './components/pages/homePage.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/homePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Directory from "../directory/directory-menu.jsx";
+import './homePage.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <Directory />
+        </div>
+    </div>
+
+);
+
+export default HomePage;
